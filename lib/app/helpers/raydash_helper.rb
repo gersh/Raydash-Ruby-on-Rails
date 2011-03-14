0921a6a2d600b89d24b5a345c6b5507ee85cf4e8
Gershon Bialer <gershon.bialer@gmail.com>
2011-03-13 23:13:38 -0500

    Improved functionality of hiding camera

--- lib/app/helpers/raydash_helper.rb
+++ lib/app/helpers/raydash_helper.rb
@@ -3,11 +3,11 @@ module RaydashHelper
     javascript_include_tag 'swfobject.js'
   end
   # This elements plays video/audio from the server
-  def videostream(token, id,width="640",height="480",hideCamera="0")
-    javascript_tag("swfobject.embedSWF(\"/swfs/raydash/ClientBox.swf\", \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{autostart:1,token:\"#{token}\",hideCamera:\"#{hideCamera}\"})")  +  content_tag(:div, "Video stream not available", :id => id)
+  def videostream(token, id,width="640",height="480",videoOff="0")
+    javascript_tag("swfobject.embedSWF(\"/swfs/raydash/ClientBox.swf\", \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{autostart:1,token:\"#{token}\",videoOff:\"#{videoOff}\"})")  +  content_tag(:div, "Video stream not available", :id => id)
   end
   # This element sends video/audio to the server
-  def videorecord(token, id,width="640",height="480",videoOff="0")
-    javascript_tag("swfobject.embedSWF(\"/swfs/raydash/RecordBox.swf\", \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{hideControls:1,hideCamera:0,autostart:1,token:\"#{token}\",videoOff:\"#{videoOff}\"})") +  content_tag(:div, "Video stream not available", :id => id)
+  def videorecord(token, id,width="640",height="480",hideCamera="0",videoOff="0")
+    javascript_tag("swfobject.embedSWF(\"/swfs/raydash/RecordBox.swf\", \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{hideControls:1,hideCamera:\"#{hideCamera}\",autostart:1,token:\"#{token}\",videoOff:\"#{videoOff}\"})") +  content_tag(:div, "Video stream not available", :id => id)
   end
 end
