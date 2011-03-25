20ceade2d0c0a24a831662a132f53675fd4d585a
Gershon Bialer <gershon.bialer@gmail.com>
2011-03-25 01:03:51 -0500

    Updated gem to resize

--- lib/app/helpers/raydash_helper.rb
+++ lib/app/helpers/raydash_helper.rb
@@ -8,6 +8,6 @@ module RaydashHelper
   end
   # This element sends video/audio to the server
   def videorecord(token, id,width="640",height="480",hideCamera="0",videoOff="0")
-    javascript_tag("swfobject.embedSWF(\"/swfs/raydash/RecordBox.swf\", \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{hideControls:1,hideCamera:\"#{hideCamera}\",autostart:1,token:\"#{token}\",videoOff:\"#{videoOff}\"})") +  content_tag(:div, "Video stream not available", :id => id)
+    javascript_tag("function setSize(width,height,id) {if(id==undefined){id=\"#{id}\";  }document.getElementById(id).width=width;document.getElementById(id).height=height;} swfobject.embedSWF(\"/swfs/raydash/RecordBox.swf\", \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{hideControls:1,hideCamera:\"#{hideCamera}\",autostart:1,token:\"#{token}\",videoOff:\"#{videoOff}\"});") +  content_tag(:div, "Video stream not available", :id => id)
   end
 end
