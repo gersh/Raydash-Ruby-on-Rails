cffed69ae462848eeaea123a04ef150543bc299c
Gershon Bialer <gershon.bialer@gmail.com>
2011-04-13 11:59:39 -0500

    Fixed up API and helpers for V2

--- lib/app/helpers/raydash_helper.rb
+++ lib/app/helpers/raydash_helper.rb
@@ -4,10 +4,10 @@ module RaydashHelper
   end
   # This elements plays video/audio from the server
   def videostream(token, id,width="640",height="480",videoOff="0")
-    javascript_tag("swfobject.embedSWF(\"/swfs/raydash/ClientBox.swf\", \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{autostart:1,token:\"#{token}\",videoOff:\"#{videoOff}\"})")  +  content_tag(:div, "Video stream not available", :id => id)
+    javascript_tag("swfobject.embedSWF(\"http://" +  Raydash.RAYDASH_HTTP_SERVER + ":" + Raydash.RAYDASH_HTTP_PORT + "/clientbox/" + Raydash.CLIENTBOX_VERSION + "\", \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{autostart:1,token:\"#{token}\",videoOff:\"#{videoOff}\"})")  +  content_tag(:div, "Video stream not available", :id => id)
   end
   # This element sends video/audio to the server
   def videorecord(token, id,width="640",height="480",hideCamera="0",videoOff="0")
-    javascript_tag("function setSize(width,height,id) {if(id==undefined){id=\"#{id}\";  }document.getElementById(id).width=width;document.getElementById(id).height=height;} swfobject.embedSWF(\"/swfs/raydash/RecordBox.swf\", \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{hideControls:1,hideCamera:\"#{hideCamera}\",autostart:1,token:\"#{token}\",videoOff:\"#{videoOff}\"});") +  content_tag(:div, "Video stream not available", :id => id)
+    javascript_tag("function setSize(width,height,id) {if(id==undefined){id=\"#{id}\";  }document.getElementById(id).width=width;document.getElementById(id).height=height;} swfobject.embedSWF(\"http://" +  Raydash.RAYDASH_HTTP_SERVER + ":" + Raydash.RAYDASH_HTTP_PORT + "/clientbox/" + Raydash.CLIENTBOX_VERSION + "\", \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{hideControls:1,hideCamera:\"#{hideCamera}\",autostart:1,token:\"#{token}\",videoOff:\"#{videoOff}\"});") +  content_tag(:div, "Video stream not available", :id => id)
   end
 end
--- lib/raydash.rb
+++ lib/raydash.rb
@@ -4,6 +4,8 @@ require 'json'
 module Raydash
   RAYDASH_HTTP_SERVER = "api.raydash.com"
   RAYDASH_HTTP_PORT = 8080
+  CLIENTBOX_VERSION = "1"
+  RECORDBOX_VERSION = "2"
   mattr_accessor :userid
   mattr_accessor :secret
   self.userid = ENV['RAYDASH_USERID']
@@ -11,46 +13,55 @@ module Raydash
 
 
   # Gets a new token for internal or external streams. The same token can be re-used for input and output streams.
-  def self.getToken(streamName="")
+  def self.getToken(streamName="", echoGroup="",customGroup="")
     if streamName!="" then
-       path="/api/1/authtoken/" + streamName + "?userid=" + self.userid + "&secret=" + self.secret
+      #       path="/api/2/authtoken/" + streamName + "?userid=" + self.userid + "&secret=" + self.secret
+      result=self.postRequest("/api/2/authtoken",{'userid'=>self.userid,'secret'=>self.secret,'streamName'=>streamName,'echoGroup'=>echoGroup,'customGroup'=>customGroup,'streamName'=>streamName})
     else
-       path="/api/1/authtoken"  + "?userid=" + self.userid + "&secret=" + self.secret
+      # path="/api/2/authtoken"  + "?userid=" + self.userid + "&secret=" + self.secret
+      result=self.postRequest("/api/2/authtoken",{'userid'=>self.userid,'secret'=>self.secret,'streamName'=>streamName,'echoGroup'=>echoGroup,'customGroup'=>customGroup})
     end
-    result = self.doRequest(path)
-    result.value()
     return result.body
   end
   # Changes which input-stream connects to which output-stream token
   def self.changeStream(output_token,input_token)
-    path="/api/1/changeStream/" + output_token + "/" + input_token + "?userid=" + self.userid + "&secret=" + self.secret
-    result = doRequest(path)
-    result.value()
+    result = postRequest("/api/2/authtoken/#{output_token}",{'streamName'=>input_token, 'userid' => self.userid, 'secret' => self.secret, })
+    return result.body
+  end
+  def self.changeStreamNext(output_token,customGroup="",active="",echoGroup="")
+    result = postRequest("/api/2/authtoken/#{output_token}/next?customGroup=#{customGroup}&active=#{active}&echoGroup=#{activeGroup}")
+    return result.body
+  end
+
+  def self.changeStreamRand(output_token,input_token,customGroup="",active="",echoGroup="")
+    result = postRequest("/api/2/authtoken/#{output_token}/randnext?echoGroup=#{echoGroup}&customGroup=#{customGroup}&active=#{active}")
     return result.body
   end
   # Gets information about the stream
   def self.getStreamInfo(token)
-    path="/api/1/streamInfo/" + token + "?userid=" + self.userid + "&secret=" + self.secret
-    result = doRequest(path)
-    result.value()
+    path="/api/2/authtoken/#{token}" + token + "?userid=" + self.userid + "&secret=" + self.secret
+    result = getRequest(path)
     return JSON.parse(result.body)
   end
   # Sets a callback url with three parameters
   def self.setCallbackUrl(userid, secret, url)
-    path="/api/1/callbackurl/" + self.userid + "?userid=" + self.userid  + "&secret=" + self.secret + "&url=" + CGI::escape(url)
+    path="/api/2/callbackurl/"  + "?userid=" + self.userid  + "&secret=" + self.secret + "&url=" + CGI::escape(url)
     result = doRequest(path)
     result.value()
     return JSON.parse(result.body)
   end
+
   # Used for configuration
   def self.setup
     yield self
   end
 
   private
-  def self.doRequest(request)
+  def self.getRequest(request)
     return Net::HTTP.get_response(RAYDASH_HTTP_SERVER, request, RAYDASH_HTTP_PORT)
   end
-
+  def self.postRequest(request)
+    return Net::HTTP.post_form("http://" + RAYDASH_HTTP_SERVER + ":" + RAYDASH_HTTP_PORT.to_s(), request)
+  end
 end
 ActionView::Base.send :include, RaydashHelper
