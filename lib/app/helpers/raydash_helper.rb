8c222f3ffbfdbcdca4fbd09a6ef4882cb2409463
Gershon Bialer <gershon.bialer@gmail.com>
2011-08-30 15:48:22 -0500

    Adding autostart parameter to helper

--- lib/app/helpers/raydash_helper.rb
+++ lib/app/helpers/raydash_helper.rb
@@ -5,12 +5,12 @@ module RaydashHelper
   end
 
   # This elements plays video/audio from the server
-  def videostream(token, id,width="640",height="480",videoOff="0")
-    javascript_tag("swfobject.embedSWF(\"" + Raydash.getAssetUrl() + "/api/2/clientbox/1\", \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{autostart:1,token:\"#{token}\",videoOff:\"#{videoOff}\"},{allowscriptaccess:'always'},{});")  +  content_tag(:div, "Video stream not available", :id => id)
+  def videostream(token, id,width="640",height="480",videoOff="0",autostart="1")
+    javascript_tag("swfobject.embedSWF(\"" + Raydash.getAssetUrl() + "/api/2/clientbox/1\", \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{autostart:#{autostart},token:\"#{token}\",videoOff:\"#{videoOff}\"},{allowscriptaccess:'always'},{});")  +  content_tag(:div, "Video stream not available", :id => id)
   end
   # This element sends video/audio to the server
-  def videorecord(token, id,width="640",height="480",hideCamera="0",videoOff="0",microphoneOff="0")
-    javascript_tag("function setSize(width,height,id) {if(id==undefined){id=\"#{id}\";  }document.getElementById(id).width=width;document.getElementById(id).height=height;}\n"   + "\nvar server_version=2;\nvar version=swfobject.getFlashPlayerVersion();if(version.major<10 || version.minor<3) {server_version=1;}"  + "else {server_version=2;}\nswfobject.embedSWF(\"" + Raydash.getAssetUrl() +  "/api/2/recordbox/\" + server_version , \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{hideControls:1,hideCamera:\"#{hideCamera}\",autostart:1,token:\"#{token}\",videoOff:\"#{videoOff}\",microphoneOff:\"#{microphoneOff}\"},{allowscriptaccess:'always'},{});" ) +  content_tag(:div, "Video stream not available", :id => id)
+  def videorecord(token, id,width="640",height="480",hideCamera="0",videoOff="0",microphoneOff="0",autostart="1")
+    javascript_tag("function setSize(width,height,id) {if(id==undefined){id=\"#{id}\";  }document.getElementById(id).width=width;document.getElementById(id).height=height;}\n"   + "\nvar server_version=2;\nvar version=swfobject.getFlashPlayerVersion();if(version.major<10 || version.minor<3) {server_version=1;}"  + "else {server_version=2;}\nswfobject.embedSWF(\"" + Raydash.getAssetUrl() +  "/api/2/recordbox/\" + server_version , \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{hideControls:1,hideCamera:\"#{hideCamera}\",autostart:#{autostart},token:\"#{token}\",videoOff:\"#{videoOff}\",microphoneOff:\"#{microphoneOff}\"},{allowscriptaccess:'always'},{});" ) +  content_tag(:div, "Video stream not available", :id => id)
   end
   def video_selector(record_id,video_selector_id ="video_selector_id")
     javascript_tag(<<-CODE
