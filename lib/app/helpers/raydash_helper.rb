000f0ff5c81974ec66566f40c52e8e5ed8c940e4
Gershon Bialer <gershon.bialer@gmail.com>
2011-10-14 10:28:13 -0500

    Fixed helper

--- lib/app/helpers/raydash_helper.rb
+++ lib/app/helpers/raydash_helper.rb
@@ -6,7 +6,7 @@ module RaydashHelper
 
   # This elements plays video/audio from the server
   def videostream(token, id,width="640",height="480",videoOff="0",autostart="1")
-    javascript_tag("swfobject.embedSWF(\"" + Raydash.getAssetUrl() + "/api/2/clientbox/1\", \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{autostart:#{autostart},token:\"#{token}\",videoOff:\"#{videoOff}\"},{allowscriptaccess:"always",wmode:"transparent"},{});")  +  content_tag(:div, "Video stream not available", :id => id)
+    javascript_tag("swfobject.embedSWF(\"" + Raydash.getAssetUrl() + "/api/2/clientbox/1\", \"#{id}\",#{width},#{height},\"9.0.0\",\"http://www.adobe.com/products/flashplayer/download\",{autostart:#{autostart},token:\"#{token}\",videoOff:\"#{videoOff}\"},{allowscriptaccess:'always',wmode:'transparent'},{});")  +  content_tag(:div, "Video stream not available", :id => id)
   end
   # This element sends video/audio to the server
   def videorecord(token, id,width="640",height="480",hideCamera="0",videoOff="0",microphoneOff="0",autostart="1")
